de5445b1b300cc89ca86e83ba3394c079be40be2
SWAPNIL TYAGI <swapniltyagianfield@gmail.com>
2025-05-28 17:12:47 +0530

    printing ADC value through usart

--- 9_adc/sourceDir/main.c
+++ 9_adc/sourceDir/main.c
@@ -1,18 +1,22 @@
 #include "stm32f4xx.h"
 #include "adc.h"
+#include "usart.h"
 
 uint32_t sensorValue;
 
 void _init(){}
 
-
 int main(void){
 
     initialise();
+    usart2TxInit();
+    
 
     while(1){
-         startConversion();
+        startConversion();
         sensorValue = adcRead();
+        printf("Sensor Value : %lu\n\r",sensorValue);
+
     }
 
     return 0;
